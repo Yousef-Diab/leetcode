4ea8fb0826e9199af5de1b662ad2d0bf362a0eb6
Yousef Diab <37383782+Barghouthi-Yousef@users.noreply.github.com>
2022-04-12 23:14:18 +0300

    Time: 17 ms (17.08%), Space: 6 MB (26.39%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    #define ll long long
+    int mySqrt(int x) {
+        if (x < 2)return x;
+	ll i;
+        for (i = 0; i * i <= x; i++) {
+
+	}
+	return i - 1;
+    }
+};
