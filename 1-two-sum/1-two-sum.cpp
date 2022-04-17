de764864376225ee19dfd2207bb19daaff3b6aec
Yousef Diab <37383782+Barghouthi-Yousef@users.noreply.github.com>
2022-04-18 02:38:42 +0300

    Time: 381 ms (32.69%), Space: 10.2 MB (86.96%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> cache;
-        vector<int> answer;
-        
-        for (size_t i = 0; i < nums.size(); ++i)
-        {
-            int needed_num = target - nums[i];
-            
-            if (cache.find(needed_num) != cache.end())
-            {
-                // We found it
-                answer.push_back(cache[needed_num]);
-                answer.push_back(i);
-                return answer;
-            }
-            else
-            {
-                // Didn't find it
-                cache.insert(make_pair(nums[i], i));
-            }
-        }
-        return answer;
+                for(int i=0;i<nums.size();i++)
+                for(int j = i+1; j < nums.size(); j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        vector<int> arr={i,j};
+                        return arr;
+                    }
+                    
+                }
+        vector<int> arr={};
+        return arr;
+
     }
 };
