b18c0a1125015eb76105b649eac0df3f94c6e646
Yousef Diab <37383782+Barghouthi-Yousef@users.noreply.github.com>
2022-04-22 03:38:47 +0300

    Time: 332 ms (14.18%), Space: 208.3 MB (14.49%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -17,11 +17,3 @@ public:
         v[key]=-1;
     }
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
