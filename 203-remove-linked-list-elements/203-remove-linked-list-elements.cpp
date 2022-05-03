514946a1297bc065e17fc08127dccfbb982a3016
Yousef Diab <37383782+Yousef-Diab@users.noreply.github.com>
2022-05-03 12:44:24 +0300

    Time: 43 ms (17.08%), Space: 15 MB (47.79%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -14,12 +14,9 @@ public:
         if(head==NULL)return head;
         while(head!=NULL&&head->val==val){head=head->next;}
         ListNode*curr=head;
-        while(curr!=NULL&&curr->next!=NULL){
-        if(curr->next->val==val){
-            curr->next=curr->next->next;
-        }
-        else curr=curr->next;
-        }
+        while(curr!=NULL&&curr->next!=NULL)
+        curr->next->val==val?curr->next=curr->next->next:curr=curr->next;
+        
         return head;
     }
 };
