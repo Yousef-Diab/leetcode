ed0aa510c6ebe9804a5b9b5392a449e30a40d891
Yousef Diab <37383782+Yousef-Diab@users.noreply.github.com>
2022-05-04 11:33:09 +0300

    Time: 177 ms (61.93%), Space: 72.9 MB (16.47%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<k){
-                mp[nums[i]]++;
-            }
+        unordered_map<int,int> m;int cnt=0;
+        for(int n:nums){
+            if(m[k-n]){m[k-n]--;cnt++;}
+            else m[n]++;
         }
-        int cnt=0;
-        for(int i=0;i<nums.size();i++){
-                if(nums[i]==k/2&&k%2==0){cnt+=floor(mp[nums[i]]/2);mp[nums[i]]=0;}
-                else if(mp[nums[i]]&&mp[k-nums[i]]){
-                    cnt+=min(mp[nums[i]],mp[k-nums[i]]);
-                    if(mp[nums[i]]>mp[k-nums[i]]){mp[nums[i]]-=mp[k-nums[i]];mp[k-nums[i]]=0;}
-                    else if(mp[nums[i]]<mp[k-nums[i]]){mp[k-nums[i]]-=mp[nums[i]];mp[nums[i]]=0;}
-                    else{mp[nums[i]]=0;mp[k-nums[i]]=0;}
-                }
-            }
         return cnt;
+        
     }
 };
